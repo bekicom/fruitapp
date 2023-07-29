8285b564aa1de0453096e327ec9e1c2aa2b3261f
Bekzodbek Abdulhamidov <you@example.com>
2023-07-29 10:46:09 +0500

    satrt

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,6 +9,9 @@ export  function Home() {
         <NavLink to='/sign'>
 
             register
+
+
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error mollitia quaerat, saepe laboriosam quis unde ratione reprehenderit esse, quidem eos deserunt officiis omnis alias! Quasi aliquam veritatis quidem earum iure.
         </NavLink>
       
     </div>
