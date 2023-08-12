2ecd83e781a03ee5b4b3bc9ecd2517f2aedec014
Bekzodbek Abdulhamidov <you@example.com>
2023-08-12 12:00:14 +0500

    fff\

--- src/pages/Basket.js
+++ src/pages/Basket.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, {  useEffect,useState } from 'react'
 
 
 import './all.css'
@@ -10,39 +10,88 @@ export function Basket() {
         JSON.parse(localStorage.getItem('food')) || []
     )
 
+    // umumiy narx uchun state
 
-    console.log(getdata.length > 1 ? 'bor' : 'yoq');
+    const [ tot,setTot] = useState(0)
 
-    console.log(setGetdata);
+    useEffect(() => {
+        const interval = setInterval(() => {
+          const updatedBasket = JSON.parse(localStorage.getItem('food'));
+          if (updatedBasket) {
+            setGetdata(updatedBasket);
+          }
+        }, 100); // 1000 ms = 1 sekund
+      
+        return () => {
+          clearInterval(interval);
+        };
+      }, []);
+
+
+
+
+    // bu remove functioni
     function remove(itemToRemove) {
         alert('ochirmoqchimisiz')
         const MyData = [...getdata];
-
-
         const updatedData = MyData.filter(item => item !== itemToRemove);
-
         setGetdata(updatedData);
         localStorage.setItem("food", JSON.stringify(updatedData));
     }
 
+    //plus functioni
+
+
+    const plus =(id)=>{
+        const yangidata = [...getdata]
+        yangidata.map((item)=>{
+            if(item.id === id){
+               item.count = item.count+1
+               localStorage.setItem('food',JSON.stringify(yangidata))
+            }
+        })
+
+
+    }
+// minus function
+    const minus =(id)=>{
+        const yangidata = [...getdata]
+        yangidata.map((item)=>{
+            if(item.id === id){
+               item.count = item.count>=1? item.count-1 :item.count
+               localStorage.setItem('food',JSON.stringify(yangidata))
+            }
+        })
+    }
+
+
+
+    /// umumiy narx
+    useEffect(()=>{
+        let total = 0;
+        getdata.map((item)=>{
+          total += item.narxi * item.count;
+          return setTot(total)
+        })
+      },[plus])
+
     return (
         <div className='basket'  >
+            
   <p>{getdata.length >= 1?'':'malumot yoq'}</p>
-         
+ 
             {getdata.map((item) => (
                 <div className='cart_basket'   >
                     <img src={item.img} alt="" />
                     <p>{item.nomi}</p>
-                    <span>{item.narxi}</span>
+                    <span>{item.narxi * item.count+'$'}</span>
+
+                    <button  onClick={()=> minus(item.id)}>-</button>
+                    <span>{item.count}</span>
+                    <button  onClick={()=>plus(item.id)}>+</button>
 
                     {/* <button onClick={locol.bind(this, item)}>add</button> */}
-                    <select name="" id="">
-                        <option value="">1</option>
-                        <option value="">2</option>
-                        <option value="">3</option>
-                        <option value="">4</option>
-                        <option value="">5</option>
-                    </select>
+                   
                     <button onClick={() => remove(item)}><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M3 6H5H21" stroke="red" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                         <path d="M8 6V4C8 3.46957 8.21071 2.96086 8.58579 2.58579C8.96086 2.21071 9.46957 2 10 2H14C14.5304 2 15.0391 2.21071 15.4142 2.58579C15.7893 2.96086 16 3.46957 16 4V6M19 6V20C19 20.5304 18.7893 21.0391 18.4142 21.4142C18.0391 21.7893 17.5304 22 17 22H7C6.46957 22 5.96086 21.7893 5.58579 21.4142C5.21071 21.0391 5 20.5304 5 20V6H19Z" stroke="red" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
@@ -50,8 +99,10 @@ export function Basket() {
                         <path d="M14 11V17" stroke="red" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                     </svg>
                     </button>
+                 
                 </div>
             ))}
+             <span>{tot}</span>
         </div>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, {   useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import { ToastContainer, toast } from 'react-toastify';
 
 import img1 from '../assets/home/1.png'
@@ -9,7 +9,7 @@ import img5 from '../assets/home/5.png'
 import img6 from '../assets/home/6.png'
 import 'react-toastify/dist/ReactToastify.css';
 
-export  function Home() {
+export function Home() {
   const [basket, setBasket] = useState(
     JSON.parse(localStorage.getItem("food")) || []
   );
@@ -24,34 +24,34 @@ export  function Home() {
 
   const data = [
 
-    {img:img1,id:1, nomi:'Pineapple',narxi:955},
-    {img:img2,id:2, nomi:'Banana',narxi:998},
-    {img:img3,id:3, nomi:'Green Grapes',narxi:998},
-    {img:img4,id:4, nomi:'Grapes',narxi:998},
-    {img:img5,id:5, nomi:'Melon',narxi:998},
-    {img:img6,id:6, nomi:'Mango',narxi:998},
-    {img:img4,id:7, nomi:'Grapes',narxi:998},
-    {img:img5,id:8, nomi:'Melon',narxi:998},
-    {img:img6,id:9, nomi:'Mango',narxi:998},
-    {img:img4,id:10, nomi:'Grapes',narxi:998},
-    {img:img5,id:11, nomi:'Melon',narxi:998},
-    {img:img6,id:12, nomi:'Mango',narxi:998},
+    { img: img1, id: 1,  count:1, nomi: 'Pineapple', narxi: 500 },
+    { img: img2, id: 2,  count:1, nomi: 'Banana', narxi: 500 },
+    { img: img3, id: 3,  count:1, nomi: 'Green Grapes', narxi: 500 },
+    { img: img4, id: 4,  count:1, nomi: 'Grapes', narxi: 500 },
+    { img: img5, id: 5,  count:1, nomi: 'Melon', narxi: 500 },
+    { img: img6, id: 6,  count:1, nomi: 'Mango', narxi: 500 },
+    { img: img4, id: 7,  count:1, nomi: 'Grapes', narxi: 500 },
+    { img: img5, id: 8,  count:1, nomi: 'Melon', narxi: 500 },
+    { img: img6, id: 9,  count:1, nomi: 'Mango', narxi: 500 },
+    { img: img4, id: 10,  count:1, nomi: 'Grapes', narxi: 500 },
+    { img: img5, id: 11,  count:1, nomi: 'Melon', narxi: 500 },
+    { img: img6, id: 12,  count:1, nomi: 'Mango', narxi: 500 },
   ]
 
-   
- 
 
 
-   
 
-   
+
+
+
+
   function locol(item) {
 
     const MyData = [...basket];
-    
+
     let test = true;
 
-  
+
 
     if (test) {
       MyData.push(item);
@@ -62,35 +62,50 @@ export  function Home() {
     notify()
 
   }
-  
 
+
+   // obnavit
+   useEffect(() => {
+    const interval = setInterval(() => {
+      const updatedBasket = JSON.parse(localStorage.getItem('food'));
+      if (updatedBasket) {
+        setBasket(updatedBasket);
+      }
+    }, 100); // 1000 ms = 1 sekund
   
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+
+
 
   return (
     <div className='products' >
-  
-      {data.map((item,index)=>(
-          <div className='cart'   >
-            <img src={item.img} alt="" />
-            <p>{item.nomi}</p>
-            <span>{item.narxi}</span>
 
-            <button onClick={locol.bind(this, item,)}>add</button>
-            <ToastContainer position="top-right"
-autoClose={2000}
-hideProgressBar={false}
-newestOnTop={false}
-closeOnClick
-rtl={false}
-pauseOnFocusLoss
-draggable
-pauseOnHover
-theme="light" />
-     
-             </div>
+      {data.map((item, index) => (
+        <div className='cart'   >
+          <img src={item.img} alt="" />
+          <p>{item.nomi}</p>
+          <span>{item.narxi}</span>
+
+          <button onClick={locol.bind(this, item,)}>add</button>
+          <ToastContainer position="top-right"
+            autoClose={2000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light" />
+
+        </div>
       ))}
 
-   
+
 
 
     </div>
